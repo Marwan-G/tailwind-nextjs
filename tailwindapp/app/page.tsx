e217417bbbe6c20cd0f3074f4fa1665347941902
Marwan-G <ghabin2004@gmail.com>
2024-01-02 17:38:55 +0000

    update component

--- tailwindapp/app/page.tsx
+++ tailwindapp/app/page.tsx
@@ -2,12 +2,12 @@ import { text } from "stream/consumers";
 
 export default function Home() {
     return (
-        <div className="p-8 bg-indigo-500 max-w-sm mx-auto hover:bg-green-400 bg-white rounded-xl shadow-lg items-center flex">
+        <div className="max-w-sm mx-auto hover:bg-green-400 bg-white rounded-xl shadow-lg items-center flex mt-28 p-2">
             <div className="shrink-0">
             </div>
             <div>
-                <div className="text-xl font-medium text-black">ChitChat</div>
-                <p className="text-slate-500">You have a new message!</p>
+                <div className="text-xl font-medium text-black pb-8 pl-8">ChitChat</div>
+                <p className="text-slate-500 pb-8 pl-8">You have a new message!</p>
             </div>
         </div>)
 }
